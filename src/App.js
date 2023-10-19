78f348f7779b168b628aea55312e00ad78c1500d
dzakiyaalfyatunnufus02 <dzakiyaalfyatunnufus@gmail.com>
2023-10-19 16:17:04 +0700

    sewa-ruang

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sambutan from "./components/Sambutan";
+import Home from "./components/Home";
+import PrivateRoute from "./privat-router/PrivatRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+       
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/sambutan"
+            element={
+              <PrivateRoute>
+                <Sambutan />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
